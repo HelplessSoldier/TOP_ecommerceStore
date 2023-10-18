8667606c6803c44eb40250177b81729a9af6097f
HelplessSoldier <patrick1234143211@gmail.com>
2023-10-18 15:27:58 -0700

    added basic routing

--- weldingStore/src/main.tsx
+++ weldingStore/src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { LandingPage } from './landingPage/LandingPage.tsx'
+import { StorePage } from './storePage/StorePage.tsx'
+import { CartPage } from './cartPage/CartPage.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />
+  },
+  {
+    path: "/store",
+    element: <StorePage />
+  },
+  {
+    path: "/cart",
+    element: <CartPage />
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
