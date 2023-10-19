f798ad9e8ac0c8e4029039ae4904b8d542cc3f20
HelplessSoldier <patrick1234143211@gmail.com>
2023-10-19 12:55:20 -0700

    started styling

--- weldingStore/src/storePage/comps/ProductPreviewItem.tsx
+++ weldingStore/src/storePage/comps/ProductPreviewItem.tsx
@@ -1,18 +1,23 @@
 import { Product } from "../../types";
+import "./ProductPreviewItem.css";
 
 export function ProductPreviewItem(props: { product: Product }) {
   const { name, description, productDetails, imgSrc } = props.product;
 
   return (
     <div className="productPreviewItem">
-      <img src={imgSrc} />
-      <h2>{name}</h2>
-      <p>{description}</p>
-      <ul>
-        <li>Material: {productDetails.material}</li>
-        <li>Lining: {productDetails.lining}</li>
-        <li>Made in: {productDetails.madeIn}</li>
-      </ul>
+      <div className="productImageContainer" >
+        <img src={imgSrc} className="productImage" />
+      </div>
+      <div className="productDescContainer" >
+        <h2>{name}</h2>
+        <p>{description}</p>
+        <ul>
+          <li>Material: {productDetails.material}</li>
+          <li>Lining: {productDetails.lining}</li>
+          <li>Made in: {productDetails.madeIn}</li>
+        </ul>
+      </div>
     </div>
   );
 }
