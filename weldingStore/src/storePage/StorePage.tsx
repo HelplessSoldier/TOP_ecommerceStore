1aa8d048f3e8c87b66ea983e1bf26e305b3bfaa0
HelplessSoldier <patrick1234143211@gmail.com>
2023-10-19 16:31:46 -0700

    eod

--- weldingStore/src/storePage/StorePage.tsx
+++ weldingStore/src/storePage/StorePage.tsx
@@ -1,11 +1,13 @@
 import "./StorePage.css";
 import { Products } from "./comps/Products";
 import { HeaderBar } from "../crossPageComps/HeaderBar";
+import { SaleHero } from "./comps/SaleHero";
 
 export function StorePage() {
   return (
     <>
       <HeaderBar />
+      <SaleHero />
       <div className="productsContainer">
         <Products />
       </div>
---
+++ weldingStore/src/storePage/comps/SaleHero.tsx
@@ -0,0 +1,10 @@
+import "./SaleHero.css"
+
+export function SaleHero() {
+  return (
+    <div className="saleHeroContainer">
+      <h1 className="saleText1">SALE</h1>
+      <h1 className="saleText2">10% OFF WHEN PURCHASING WHOLE SET</h1>
+    </div>
+  )
+}
