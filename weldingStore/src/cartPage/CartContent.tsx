ad5dc435aae46f1774b195064b2fafbfc22ad2cb
HelplessSoldier <patrick1234143211@gmail.com>
2023-10-23 15:47:40 -0700

    more work on cart

--- weldingStore/src/cartPage/CartContent.tsx
+++ weldingStore/src/cartPage/CartContent.tsx
@@ -1,16 +1,25 @@
 import { useContext } from "react"
 import { CartContext } from "../App"
+import { Product } from "../types";
 
 export function CartContent() {
-  const { cart } = useContext(CartContext);
+  const { cart, setCart } = useContext(CartContext);
+
+  const handleRemoveButton = (itemToRemove: Product) => {
+    const newCart = cart.filter((item) => item != itemToRemove);
+    setCart(newCart);
+  }
 
   return (
     <div className="cartContentContainer" >
       {cart.map((item) => {
         return (
           <div className="cartContentItem">
-            <img src={item.imgSrc} />
-            <h2>{item.name}</h2>
+            <img className="cartImage" src={item.imgSrc} />
+            <div className="cartItemText" >
+              <p>{item.name}</p>
+              <p>${item.price}</p>
+            </div>
           </div>
         )
       })}
