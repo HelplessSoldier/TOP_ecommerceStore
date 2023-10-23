34a2046764f076f357e4afb4adecef5e641cb625
HelplessSoldier <patrick1234143211@gmail.com>
2023-10-23 12:15:18 -0700

    uncomitted changes

--- weldingStore/src/storePage/comps/SaleHero.tsx
+++ weldingStore/src/storePage/comps/SaleHero.tsx
@@ -4,7 +4,7 @@ export function SaleHero() {
   return (
     <div className="saleHeroContainer">
       <h1 className="saleText1">SALE</h1>
-      <h1 className="saleText2">10% OFF WHEN PURCHASING WHOLE SET</h1>
+      <h1 className="saleText2">10% OFF WHEN PURCHASING WHOLE SET*</h1>
     </div>
   )
 }
