4fcfba9d784233e235a7c2a58a461bbf1a4c6fa8
HelplessSoldier <patrick1234143211@gmail.com>
2023-10-24 14:54:10 -0700

    added shop button to Landing page

--- weldingStore/src/landingPage/Hero.tsx
+++ weldingStore/src/landingPage/Hero.tsx
@@ -1,11 +1,24 @@
 import "./Hero.css";
+import { useNavigate } from "react-router-dom";
 
 export function Hero() {
+  const navigate = useNavigate();
+
+  const handleStoreButtonClick = () => {
+    navigate("/store");
+  }
+
   return (
     <div className="heroContainer">
       <h1 className="heroHeader">
-        lorem ipsum
+        Clean drip for a dirty job.
       </h1>
+      <button
+        className="shopNowButton"
+        onClick={() => handleStoreButtonClick()}
+      >
+        Shop
+      </button>
     </div>
   )
 }
