c9f63e769e556da8438fcb26932384f38685ba37
HelplessSoldier <patrick1234143211@gmail.com>
2023-10-24 14:44:25 -0700

    added empty cart text

--- weldingStore/src/cartPage/CartPage.tsx
+++ weldingStore/src/cartPage/CartPage.tsx
@@ -1,18 +1,28 @@
+import "./CartPage.css";
+import { useContext } from "react";
 import { HeaderBar } from "../crossPageComps/HeaderBar";
 import { CartContent } from "./CartContent";
+import { CartContext } from "../App";
 
 export function CartPage() {
+  const { cart } = useContext(CartContext);
   return (
     <>
       <HeaderBar />
-      <div style={{
-        display: "flex",
-        width: "100vw",
-        alignItems: "center",
-        justifyContent: "center"
-      }}>
-        <CartContent />
-      </div>
+      {cart.length > 0 ?
+        <div style={{
+          display: "flex",
+          width: "100vw",
+          alignItems: "center",
+          justifyContent: "center"
+        }}>
+          <CartContent />
+        </div>
+        :
+        <h1 className="emptyCartText">
+          Cart is Empty
+        </h1>
+      }
     </>
   )
 }
